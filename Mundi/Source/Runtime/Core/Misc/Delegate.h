a7dd958136088253b4a9b418f64258c344027df6
Teterschaft <gofla2242@tukorea.ac.kr>
2025-10-31 16:25:28 +0900

    feat: Delegate 시스템 구현
      구현 기능
    
      TDelegate<Args...> - 단일 함수 바인딩
    
      - 람다, 함수 포인터, std::function 바인딩 지원
      - 멤버 함수 바인딩 (BindMember)
      - 바인딩 해제 (Unbind)
      - 바인딩 여부 확인 (IsBound)
      - 함수 실행 (Execute 및 operator())
    
      TMulticastDelegate<Args...> - 여러 함수 바인딩
    
      - 여러 함수를 리스트로 관리
      - 핸들 기반 추가/제거 (Add, Remove)
      - 멤버 함수 추가 (AddMember)
      - 모든 함수 브로드캐스트 (Broadcast 및 operator())
      - 전체 초기화 (Clear)
    
      언리얼 엔진 스타일 매크로
    
      // Single Delegate
      DECLARE_DELEGATE(DelegateName)
      DECLARE_DELEGATE_OneParam(DelegateName, Param1Type)
      DECLARE_DELEGATE_TwoParams(DelegateName, Param1Type, Param2Type)
      // ... FourParams까지
    
      // Multicast Delegate
      DECLARE_MULTICAST_DELEGATE(DelegateName)
      DECLARE_MULTICAST_DELEGATE_OneParam(DelegateName, Param1Type)
      DECLARE_MULTICAST_DELEGATE_TwoParams(DelegateName, Param1Type, Param2Type)
      // ... FiveParams까지

---
+++ Mundi/Source/Runtime/Core/Misc/Delegate.h
@@ -0,0 +1,186 @@
+﻿#pragma once
+
+#include <functional>
+#include <memory>
+#include "UEContainer.h"
+
+// 단일 함수 바인딩을 위한 Delegate
+template<typename... Args>
+class TDelegate
+{
+public:
+	using HandlerType = std::function<void(Args...)>;
+
+	TDelegate() = default;
+
+	// 람다, 함수 포인터, std::function 바인딩
+	void Bind(HandlerType InFunction)
+	{
+		Function = InFunction;
+	}
+
+	// 멤버 함수 바인딩
+	template<typename TObject>
+	void BindMember(TObject* InObject, void (TObject::*InMethod)(Args...))
+	{
+		Function = [InObject, InMethod](Args... InArgs)
+		{
+			(InObject->*InMethod)(InArgs...);
+		};
+	}
+
+	// 바인딩 해제
+	void Unbind()
+	{
+		Function = nullptr;
+	}
+
+	// 바인딩 여부 확인
+	bool IsBound() const
+	{
+		return Function != nullptr;
+	}
+
+	// 실행
+	void Execute(Args... InArgs) const
+	{
+		if (IsBound())
+		{
+			Function(InArgs...);
+		}
+	}
+
+	// () 연산자 오버로딩
+	void operator()(Args... InArgs) const
+	{
+		Execute(InArgs...);
+	}
+
+private:
+	HandlerType Function;
+};
+
+// 여러 함수 바인딩을 위한 Multicast Delegate
+template<typename... Args>
+class TMulticastDelegate
+{
+public:
+	using HandlerType = std::function<void(Args...)>;
+	using DelegateHandle = size_t;
+
+	TMulticastDelegate() = default;
+
+	// 람다, 함수 포인터, std::function 추가
+	DelegateHandle Add(HandlerType InFunction)
+	{
+		DelegateHandle Handle = NextHandle++;
+		Functions.emplace_back(Handle, InFunction);
+		return Handle;
+	}
+
+	// 멤버 함수 추가
+	template<typename TObject>
+	DelegateHandle AddMember(TObject* InObject, void (TObject::*InMethod)(Args...))
+	{
+		auto Function = [InObject, InMethod](Args... InArgs)
+		{
+			(InObject->*InMethod)(InArgs...);
+		};
+		return Add(Function);
+	}
+
+	// 핸들로 제거
+	void Remove(DelegateHandle Handle)
+	{
+		Functions.erase(
+			std::remove_if(Functions.begin(), Functions.end(),
+				[Handle](const std::pair<DelegateHandle, HandlerType>& Pair)
+				{
+					return Pair.first == Handle;
+				}),
+			Functions.end()
+		);
+	}
+
+	// 모두 제거
+	void Clear()
+	{
+		Functions.clear();
+	}
+
+	// 바인딩 여부 확인
+	bool IsBound() const
+	{
+		return !Functions.empty();
+	}
+
+	// 모든 함수 실행
+	void Broadcast(Args... InArgs) const
+	{
+		for (const auto& Pair : Functions)
+		{
+			if (Pair.second)
+			{
+				Pair.second(InArgs...);
+			}
+		}
+	}
+
+	// () 연산자 오버로딩
+	void operator()(Args... InArgs) const
+	{
+		Broadcast(InArgs...);
+	}
+
+private:
+	TArray<TPair<DelegateHandle, HandlerType>> Functions;
+	DelegateHandle NextHandle = 0;
+};
+
+// 매크로 정의 (언리얼 스타일)
+
+// 파라미터가 없는 Delegate 선언
+#define DECLARE_DELEGATE(DelegateName) \
+	using DelegateName = TDelegate<>
+
+// 파라미터가 1개인 Delegate 선언
+#define DECLARE_DELEGATE_OneParam(DelegateName, Param1Type) \
+	using DelegateName = TDelegate<Param1Type>
+
+// 파라미터가 2개인 Delegate 선언
+#define DECLARE_DELEGATE_TwoParams(DelegateName, Param1Type, Param2Type) \
+	using DelegateName = TDelegate<Param1Type, Param2Type>
+
+// 파라미터가 3개인 Delegate 선언
+#define DECLARE_DELEGATE_ThreeParams(DelegateName, Param1Type, Param2Type, Param3Type) \
+	using DelegateName = TDelegate<Param1Type, Param2Type, Param3Type>
+
+// 파라미터가 4개인 Delegate 선언
+#define DECLARE_DELEGATE_FourParams(DelegateName, Param1Type, Param2Type, Param3Type, Param4Type) \
+	using DelegateName = TDelegate<Param1Type, Param2Type, Param3Type, Param4Type>
+
+// Multicast Delegate 매크로
+
+// 파라미터가 없는 Multicast Delegate 선언
+#define DECLARE_MULTICAST_DELEGATE(DelegateName) \
+	using DelegateName = TMulticastDelegate<>
+
+// 파라미터가 1개인 Multicast Delegate 선언
+#define DECLARE_MULTICAST_DELEGATE_OneParam(DelegateName, Param1Type) \
+	using DelegateName = TMulticastDelegate<Param1Type>
+
+// 파라미터가 2개인 Multicast Delegate 선언
+#define DECLARE_MULTICAST_DELEGATE_TwoParams(DelegateName, Param1Type, Param2Type) \
+	using DelegateName = TMulticastDelegate<Param1Type, Param2Type>
+
+// 파라미터가 3개인 Multicast Delegate 선언
+#define DECLARE_MULTICAST_DELEGATE_ThreeParams(DelegateName, Param1Type, Param2Type, Param3Type) \
+	using DelegateName = TMulticastDelegate<Param1Type, Param2Type, Param3Type>
+
+// 파라미터가 4개인 Multicast Delegate 선언
+#define DECLARE_MULTICAST_DELEGATE_FourParams(DelegateName, Param1Type, Param2Type, Param3Type, Param4Type) \
+	using DelegateName = TMulticastDelegate<Param1Type, Param2Type, Param3Type, Param4Type>
+
+// 파라미터가 5개인 Multicast Delegate 선언
+#define DECLARE_MULTICAST_DELEGATE_FiveParams(DelegateName, Param1Type, Param2Type, Param3Type, Param4Type, Param5Type) \
+	using DelegateName = TMulticastDelegate<Param1Type, Param2Type, Param3Type, Param4Type, Param5Type>
