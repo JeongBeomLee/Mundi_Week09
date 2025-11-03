299fc3d09504e7149adcb310dfc706ad0eb6c36c
changgeun <hoi000115@gmail.com>
2025-11-03 16:35:11 +0900

    feat: range 기반 순회 -> index기반 순회로 변경

--- Mundi/Source/Runtime/Engine/GameFramework/World.cpp
+++ Mundi/Source/Runtime/Engine/GameFramework/World.cpp
@@ -107,8 +107,11 @@ void UWorld::Tick(float DeltaSeconds)
 	//순서 바꾸면 안댐
 	if (Level)
 	{
-		for (AActor* Actor : Level->GetActors())
+		// Index-based iteration: Tick 중에 Actor가 추가/삭제되어도 안전
+		const TArray<AActor*>& Actors = Level->GetActors();
+		for (size_t i = 0; i < Actors.size(); ++i)
 		{
+			AActor* Actor = Actors[i];
 			// PendingKill 상태인 Actor는 Tick하지 않음
 			if (Actor && !Actor->IsPendingKill() && (Actor->CanTickInEditor() || bPie))
 			{
@@ -116,8 +119,11 @@ void UWorld::Tick(float DeltaSeconds)
 			}
 		}
 	}
-	for (AActor* EditorActor : EditorActors)
+
+	// EditorActors도 인덱스 기반 순회로 변경
+	for (size_t i = 0; i < EditorActors.size(); ++i)
 	{
+		AActor* EditorActor = EditorActors[i];
 		if (EditorActor && !EditorActor->IsPendingKill() && !bPie)
 		{
 			EditorActor->Tick(DeltaSeconds);
