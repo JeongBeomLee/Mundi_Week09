2367c075240a0a1ce144589de7bb13cd75d6c12b
changgeun <hoi000115@gmail.com>
2025-11-05 19:58:38 +0900

    feat: StandAlone모드 추가

--- Mundi/Source/Runtime/Engine/GameFramework/EditorEngine.h
+++ Mundi/Source/Runtime/Engine/GameFramework/EditorEngine.h
@@ -16,22 +16,23 @@ struct FWorldContext
     EWorldType WorldType;
 };
 
-class UEditorEngine final 
+class UEditorEngine
 {
 public:
     bool bChangedPieToEditor = false;
 
     UEditorEngine();
-    ~UEditorEngine();
+    virtual ~UEditorEngine();
 
-    bool Startup(HINSTANCE hInstance);
+    virtual bool Startup(HINSTANCE hInstance);
     void MainLoop();
     void Shutdown();
 
+    void BuildScene();
     void StartPIE();
     void EndPIE();
     bool IsPIEActive() const { return bPIEActive; }
-    
+
     HWND GetHWND() const { return HWnd; }
 
     URenderer* GetRenderer() const { return Renderer.get(); }
@@ -45,35 +46,40 @@ public:
         WorldContexts.push_back(InWorldContext);
     }
 
-private:
+
+    bool bStandAloneBuild = false;
+protected:
     bool CreateMainWindow(HINSTANCE hInstance);
     static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
     static void GetViewportSize(HWND hWnd);
 
-    void Tick(float DeltaSeconds);
+    virtual void Tick(float DeltaSeconds);
     void Render();
 
     void HandleUVInput(float DeltaSeconds);
 
-private:
     //윈도우 핸들
     HWND HWnd = nullptr;
 
     //디바이스 리소스 및 렌더러
-    D3D11RHI RHIDevice; 
+    D3D11RHI RHIDevice;
     std::unique_ptr<URenderer> Renderer;
-    
+
     //월드 핸들
     TArray<FWorldContext> WorldContexts;
 
     //틱 상태
     bool bRunning = false;
-    bool bUVScrollPaused = true;
     bool bPIEActive = false;
-    float UVScrollTime = 0.0f;
-    FVector2D UVScrollSpeed = FVector2D(0.5f, 0.5f);
 
     // 클라이언트 사이즈
     static float ClientWidth;
     static float ClientHeight;
+
+private:
+    bool bUVScrollPaused = true;
+    float UVScrollTime = 0.0f;
+    FVector2D UVScrollSpeed = FVector2D(0.5f, 0.5f);
+
+
 };
